33a21f23f8bce3aa3b5c5fae9c6dc69bc33faab8
Rithika <rith22062.ad@rmkec.ac.in>
2026-03-20 18:08:28 +0530

    added model download from gdrive

--- backend/model_service.py
+++ backend/model_service.py
@@ -1,3 +1,5 @@
+import os
+import gdown
 import torch
 import torchvision.transforms as transforms
 from PIL import Image
@@ -6,6 +8,14 @@ import torch.nn as nn
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+# -------------------------------
+# Create Model Folder if Not Exists
+# -------------------------------
+os.makedirs("models", exist_ok=True)
+
+# -------------------------------
+# Model Definition
+# -------------------------------
 class MultiModalStrokeModel(nn.Module):
     def __init__(self):
         super(MultiModalStrokeModel, self).__init__()
@@ -24,22 +34,31 @@ class MultiModalStrokeModel(nn.Module):
         )
 
     def forward(self, mri, ct):
-
         mri_feat = self.mri_model(mri)
         ct_feat = self.ct_model(ct)
 
         combined = torch.cat((mri_feat, ct_feat), dim=1)
-
         out = self.classifier(combined)
 
         return out
 
+
+# -------------------------------
+# Load Model
+# -------------------------------
 model = MultiModalStrokeModel()
 
-model.load_state_dict(
-    torch.load("models/final_multimodal_stroke_model.pth", map_location=device)
-)
+MODEL_PATH = "models/final_multimodal_stroke_model.pth"
 
+# 🔥 Download from Google Drive (ONLY FIRST TIME)
+if not os.path.exists(MODEL_PATH):
+    print("Downloading model from Google Drive...")
+
+    url = "https://drive.google.com/uc?id=13SHtYTkLJ6eqvLyGMhRGUNHAp9zid3yM"
+    gdown.download(url, MODEL_PATH, quiet=False)
+
+# Load model
+model.load_state_dict(torch.load(MODEL_PATH, map_location=device))
 model.to(device)
 model.eval()
 
@@ -47,9 +66,8 @@ model.eval()
 # -------------------------------
 # Image Transform
 # -------------------------------
-
 transform = transforms.Compose([
-    transforms.Resize((224,224)),
+    transforms.Resize((224, 224)),
     transforms.ToTensor()
 ])
 
@@ -57,7 +75,6 @@ transform = transforms.Compose([
 # -------------------------------
 # Prediction Function
 # -------------------------------
-
 def predict_stroke(mri_path, ct_path):
 
     mri = Image.open(mri_path).convert("RGB")
@@ -67,9 +84,7 @@ def predict_stroke(mri_path, ct_path):
     ct = transform(ct).unsqueeze(0).to(device)
 
     with torch.no_grad():
-
         output = model(mri, ct)
-
         prob = torch.softmax(output, dim=1)
 
     confidence = prob.max().item()
